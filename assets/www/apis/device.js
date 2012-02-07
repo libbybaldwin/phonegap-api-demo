e3849f095f1528916cb6f5392ef8db69923007df
Libby Baldwin <libby.baldwin@gmail.com>
2012-02-06 18:14:12 -0800

    minor fixes

--- assets/www/apis/device.js
+++ assets/www/apis/device.js
@@ -69,8 +69,56 @@ var onDeviceReady = function() {
     // api-camera  Photo URI
     pictureSource=navigator.camera.PictureSourceType;
     destinationType=navigator.camera.DestinationType;
+    
+    // The Samsung Galaxy Tab 10.1 is currently the only device known to
+    // support orientation/change correctly and reliably.
+    if (device.name === "GT-P7510") {
+        var updateScreen = function() {
+            document.getElementById("width").innerHTML = screen.width;
+            document.getElementById("height").innerHTML = screen.height;
+            document.getElementById("availwidth").innerHTML = screen.availWidth;
+            document.getElementById("availheight").innerHTML = screen.availHeight;        
+        };         
+        window.addEventListener("orientationchange", function(e){
+            //console.log("window.orientation: " + window.orientation);
+            updateScreen();
+        }, false);
+    }
 };
 
 function init() {
     document.addEventListener("deviceready", onDeviceReady, true);
+
+    document.getElementById('api-intro').style.display = 'block';
+    
+    var showApi = function(e) {
+        var apiId = this.id;
+        var divs = document.getElementsByClassName('api-div');   
+        for(var j=0; j<divs.length; j++) { 
+            divs[j].style.display='none';
+        }
+        var apiEl = document.getElementById('api-' + apiId);
+        apiEl.style.display = 'block';
+        scroll(0,0);
+    };
+    // add click to each api name / div
+    var apiList = document.getElementById('sidebar').getElementsByTagName('a');
+    for(var i=0; i< apiList.length; i++) { 
+        apiList[i].addEventListener('click', showApi, false);
+    }
+
+    var $select = document.getElementById('subheader').getElementsByTagName('select')[0];
+    if ($select) {
+        $select.addEventListener('change', function(e) {
+            var api = this.options[this.selectedIndex].value;
+            //alert("value: " + api);
+            
+            var divs = document.getElementsByClassName('api-div');   
+            for(var j=0; j<divs.length; j++) { 
+                divs[j].style.display='none';
+            }
+            document.getElementById('api-' + api).style.display = 'block';
+            
+        }, false);
+    } else { alert("no select here"); }
 }
--- assets/www/apis/events.js
+++ assets/www/apis/events.js
@@ -22,13 +22,11 @@ var onSearchKeyDown = function() {
     console.log("searchbutton event fired");
     document.getElementById('eventOutput').innerHTML = 
         '<span id="searchbuttontext" style="color:#2b8;"><code>searchbutton</code> fired</span>';
-    //$('#searchbuttontext').fadeOut(1500, function(){});
 };
 var onMenuButtonDown = function() {
     console.log("menubutton event fired");
     document.getElementById('eventOutput').innerHTML = 
         '<span id="menubuttontext" style="color:#2b8;"><code>menubutton</code> fired</span>';
-    //$('#menubuttontext').fadeOut(1500, function(){});
 };
 var onEventFired = function() {  // generic logging event handler
     console.log("an event fired");
